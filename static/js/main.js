f61554dbfc8400b859d94f0390c65fd7377d6399
Cristu Farace <cristianfarace@gmail.com>
2022-10-24 15:13:47 -0300

    primer commit

---
+++ static/js/main.js
@@ -0,0 +1,24 @@
+console.log("hola");
+
+// 2 A través de un callback asociado al evento DOMContentLoaded de document, escribir en la consola el texto "Contenido del DOM cargado". 
+
+
+// 3 Cargar desde JS el value del textarea con el siguiente contenido:
+// <p>Este contenido <strong>está listo</strong><br>para ser editado y pasarlo abajo.</p>
+
+// 4 Cuando se dispare el evento input del textarea, obtener un HTMLCollection con todos los <input> de la página y cambiar a false su propiedad disabled para que los mismos dejen de estar deshabilitados. Hacer lo mismo con el botón que falta.
+
+// 5 Programar los 5 botones superiores para que cumplan estas funciones:
+// Botón "Reemplazar": reemplaza el contenido del div destino por el contenido del textarea origen.
+// Botón "Agregar": agrega el contenido del textarea origen al contenido del div destino.
+// Botón "Agregar 5 veces": agrega 5 veces el contenido del textarea origen al contenido del div destino.
+// Botón "Agregar 10 veces": agrega 10 veces el contenido del textarea origen al contenido del div destino.
+// Botón "Agregar n veces": agrega la cantidad de veces que indique el usuario (a partir de un prompt) el contenido del textarea origen al contenido del div destino.
+
+
+// 6 Programar los 3 botones inferiores para que cumplan estas funciones:
+// Botón "Vaciar": vacía el contenido del div destino.
+// Botón "Convertir a mayúsculas": convierte en mayúsculas el contenido del div destino.
+// Botón "Convertir a minúsculas": convierte en minúsculas el contenido del div destino.
+
+// 7 Una vez terminados los puntos anteriores, obtener un HTMLCollection con todos los <li> de la página y agregar al inicio de su contenido el texto "[Ok] ".
